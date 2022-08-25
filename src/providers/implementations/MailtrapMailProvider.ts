93cdcd1c1e28f0764ca4f8ac176b769b073b5bfe
jeanluca554 <jeanluca554@hotmail.com>
2022-08-25 08:18:35 -0300

    feat: mail Provider Implementation
    Mail Provider implementation with mailtrap

---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,33 @@
+import { IMailProvider, IMessage } from "../ImailProvider";
+import nodemailer from 'nodemailer';
+import Mail from "nodemailer/lib/mailer";
+
+export class MailtrapMailProvider implements IMailProvider {
+    private transporter: Mail;
+
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: "smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: "bb97526c534ef3",
+                pass: "51454deaac33f6"
+            }
+        })
+    }
+
+    async sendMail(message: IMessage): Promise<void> {
+        await this.transporter.sendMail({
+            to: {
+                name: message.to.name,
+                address: message.to.email
+            },
+            from: {
+                name: message.from.name,
+                address: message.from.email
+            },
+            subject: message.subject,
+            html: message.body
+        })
+    }
+}
