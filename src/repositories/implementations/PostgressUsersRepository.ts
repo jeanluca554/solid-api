0c7c16b505b430c3eaec4ba0d0b320e817d24441
jeanluca554 <jeanluca554@hotmail.com>
2022-08-25 09:17:47 -0300

    feat: postgress repository

--- src/repositories/implementations/PostgressUsersRepository.ts
+++ src/repositories/implementations/PostgressUsersRepository.ts
@@ -5,10 +5,12 @@ export class PostgressUsersRepository implements IUsersRepository {
     private users: User[] = []; //dessa forma enquanto não temos banco de dados instalados
 
     async findByEmail(email: string): Promise<User> {
+        const user = this.users.find(user => user.email === email);
 
+        return user;
     }
 
     async save(user: User): Promise<void> {
-
+        this.users.push(user); // como se estivéssemos salvando o usuário no banco
     }
 }
