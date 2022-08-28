4b470d9061f17258aaed7740344b49b1d16f6624
jeanluca554 <jeanluca554@hotmail.com>
2022-08-28 13:31:20 -0300

    feat: add create tag route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { createUserController } from "./useCases/CreateUser";
+import { createTagController } from "./useCases/CreateTag";
 
 const router = Router();
 
 router.post('/users', (request, response) => {
     return createUserController.handle(request, response);
+});
+router.post('/tags', (request, response) => {
+    return createTagController.handle(request, response);
 });
 
 export { router };
